39ccb0f10908dcd994eeeec26cd3c3f1a16217d9
爱吃糖葫芦 <13126506430@163.com>
2015-07-22 20:59:33 +0800

    1.0.0.1
    first commit

---
+++ RenameFile/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace RenameFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbd_file = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.日期 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "重命名";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(340, 138);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // 日期
+            // 
+            this.日期.AutoSize = true;
+            this.日期.Enabled = false;
+            this.日期.Location = new System.Drawing.Point(358, 13);
+            this.日期.Name = "日期";
+            this.日期.Size = new System.Drawing.Size(47, 16);
+            this.日期.TabIndex = 4;
+            this.日期.TabStop = true;
+            this.日期.Text = "时间";
+            this.日期.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(358, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "大小";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 159);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.日期);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "重命名文件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbd_file;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton 日期;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ RenameFile/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.Devices;
+
+namespace RenameFile
+{
+    public partial class Form1 : Form
+    {
+        private FileInfo[] fiarray = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            fbd_file.ShowDialog();
+            DirectoryInfo di = new DirectoryInfo(fbd_file.SelectedPath);
+            fiarray = di.GetFiles();
+            foreach (FileInfo item in fiarray)
+            {
+                richTextBox1.Text += item.Name + "\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == MessageBox.Show(null, "确定重命名所有文件", "确定", MessageBoxButtons.OKCancel))
+            {
+                try
+                {
+                    RenameFile(fbd_file.SelectedPath);
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+
+            }
+        }
+
+        public void RenameFile(string p_strFileDir)
+        {
+            richTextBox1.Text = "";
+            foreach (FileInfo item in fiarray)
+            {
+                Computer c = new Computer();
+                string s = Guid.NewGuid().ToString();
+                c.FileSystem.RenameFile(item.FullName, s+"."+item.Extension);
+                richTextBox1.Text += s + "." + item.Extension + "\n";
+            }
+        }
+    }
+}
